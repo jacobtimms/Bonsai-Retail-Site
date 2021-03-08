47f5093628a34558d175514ecce1d82c9272fbf2
Jacob Timms <jacob_tims@live.com>
2021-03-08 18:32:09 +0000

    account.js updated

--- Bonsai/js/account.js
+++ Bonsai/js/account.js
@@ -1,17 +1,17 @@
 function validateForm1() {
-    var a = document.forms["customer_login"]["customer_email"].value;
-    var b = document.forms["customer_login"]["customer_password"].value;
-    if (a == null || a == "", b == null || b == "") {
+    var login = document.forms["customer_login"]["customer_email"].value;
+    var email = document.forms["customer_login"]["customer_password"].value;
+    if (login == null || login == "", email == null || email == "") {
       alert("Please Fill In All Required Field");
-    }else   {window.location.href='/index.html'};
+    }else   {window.location.href='./index.html'};
   }
 
 function validateForm2() {
-    var a = document.forms["create_customer"]["first_name"].value;
-    var b = document.forms["create_customer"]["last_name"].value;
-    var c = document.forms["create_customer"]["email"].value;
-    var d = document.forms["create_customer"]["password"].value;
-    if (a == null || a == "", b == null || b == "", c == null || c == "", d == null || d == "") {
+    var firstName = document.forms["create_customer"]["first_name"].value;
+    var lastName = document.forms["create_customer"]["last_name"].value;
+    var email = document.forms["create_customer"]["email"].value;
+    var password = document.forms["create_customer"]["password"].value;
+    if (firstName == null || firstName == "", lastName == null || lastName == "", email == null || email == "", password == null || password == "") {
       alert("Please Fill In All Required Field");
-    }else   {window.location.href='/index.html'};
+    }else   {window.location.href='./index.html'};
   }
